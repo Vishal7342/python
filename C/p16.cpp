3f0d45c535af6211cf630fd8a496edc6a0b04543
@vishal7342 <vishalpravinbhai6@gmail.com>
2024-07-29 01:14:31 +0530

    oop p1

---
+++ C/p16.cpp
@@ -0,0 +1,15 @@
+// WAP to print “Hello World”.
+
+#include <iostream> // Include the standard input/output stream library
+#include <conio.h> // Include the console input/output library
+
+using namespace std;
+
+int main()  // The function does not return any value
+{
+    // Clear the console screen
+    cout << "Hello World"; // Output "Hello World" to the console
+    // Wait for a keypress
+
+    return 0;
+} 
