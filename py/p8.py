49c2a13776930db35bdc46dce7b26faa1678b176
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:19:21 +0530

    Create p8.py

---
+++ py/p8.py
@@ -0,0 +1,14 @@
+'''
+Program to check if a number is odd or even:
+'''
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+# Output: 
+# 
+# Enter a number: 5676
+# Even number
