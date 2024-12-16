9b2e728e109ff5909183a8352e9f4e7a72b2bfe3
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:16:59 +0530

    Create p7.py

---
+++ py/p7.py
@@ -0,0 +1,18 @@
+'''
+Program to show single-line and multi-line string:
+'''
+
+# Single-line string
+single_line_string = "Hello, World!"
+print(single_line_string)
+
+# Multi-line string
+multi_line_string = """This is a 
+multi-line string."""
+print(multi_line_string)
+
+# Output:
+
+# Hello, World!
+# This is a 
+# multi-line string.
