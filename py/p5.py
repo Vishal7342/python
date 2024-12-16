a3408ab6722e76dc7fe178062e61ceef6f9c250b
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:15:43 +0530

    Create p5.py

---
+++ py/p5.py
@@ -0,0 +1,12 @@
+'''
+Read your name from the user and print 
+the number of characters in the name:
+'''
+
+name = input("Enter your name: ")
+print(f"Number of characters: {len(name)}")
+
+# Output:
+
+# Enter your name: vishal
+# Number of characters: 6
