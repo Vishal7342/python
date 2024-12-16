4c65274f63ef8043776547e47eb7fef598a9708f
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:19:48 +0530

    Create p9.py

---
+++ py/p9.py
@@ -0,0 +1,17 @@
+'''
+Program to print if a given number 
+is positive, negative, or zero:
+'''
+
+number = int(input("Enter a number: "))
+if number > 0:
+    print("Positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+# Output:
+# 
+# Enter a number: 45
+# Positive number
