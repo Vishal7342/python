74adf126787c678659017548bd0c842e4748a870
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:12:02 +0530

    Update p3.py

--- py/p3.py
+++ py/p3.py
@@ -1,3 +1,8 @@
+'''
+Program to print all arithmetic 
+operations using two given integers:
+'''
+
 a = 10
 b = 5
 print(f"Addition: {a + b}")
@@ -8,13 +13,12 @@ print(f"Modulus: {a % b}")
 print(f"Exponentation: {a ** b}")
 print(f"Floor Division: {a // b}")
 
-"""
-Output:
-  Addition: 15
-  Subtraction: 5
-  Multiplication: 50
-  Division: 2.0
-  Modulus: 0
-  Exponentation: 100000
-  Floor Division: 2
-"""
+# Output: 
+
+# Addition: 15
+# Subtraction: 5
+# Multiplication: 50
+# Division: 2.0
+# Modulus: 0
+# Exponentation: 100000
+# Floor Division: 2
