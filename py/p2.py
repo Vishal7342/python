f202ab0a8063cc40b41616f7733d5912c590f6d3
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:11:34 +0530

    Update p2.py

--- py/p2.py
+++ py/p2.py
@@ -1,9 +1,13 @@
-x = 10
+'''
+Program to create a variable and print 
+its value and data type:
+'''
+
+x = input("Enter the value of X: ")
 print(f"value: {x}")
 print(f"Data type: {type(x)}")
 
-"""
-output:
-  value: 10
-  Data type: <class 'int'>
-"""
+# Output:
+
+# value: 10
+# Data type: <class 'int'>
