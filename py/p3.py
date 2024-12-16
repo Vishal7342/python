7f04ccfeaf2fdeacf912750c689fd9d857c3f034
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 10:48:09 +0530

    Create p3.py

---
+++ py/p3.py
@@ -0,0 +1,20 @@
+a = 10
+b = 5
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+print(f"Modulus: {a % b}")
+print(f"Exponentation: {a ** b}")
+print(f"Floor Division: {a // b}")
+
+"""
+Output:
+  Addition: 15
+  Subtraction: 5
+  Multiplication: 50
+  Division: 2.0
+  Modulus: 0
+  Exponentation: 100000
+  Floor Division: 2
+"""
