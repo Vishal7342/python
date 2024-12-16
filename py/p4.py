1b1f58c380c8218248c6070cf86216b08ae22c4c
Kuriya Vishal Pravinbhai <155974248+Vishal7342@users.noreply.github.com>
2024-12-16 11:15:13 +0530

    Create p4.py

---
+++ py/p4.py
@@ -0,0 +1,11 @@
+'''
+Program to calculate the area of a circle:
+'''
+
+import math
+radius = 7
+area = math.pi * radius ** 2
+print(f"Area of the Circle: {area}")
+
+# Output:
+# Area of the Circle: 153.93804002589985
